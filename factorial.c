cb5483536a74cf7b08065cdde333f28e4e67428f
VISHNU M R <111166547+vishnumr18@users.noreply.github.com>
2023-01-30 21:46:25 +0530

    updating in program names

--- factorial.c
+++ factorial.c
@@ -1,18 +1,21 @@
-#include<stdio.h>
-int fact(int n){
-  if(n==0){
+// Factorial using recursion
+#include <stdio.h>
+int fact(int n)
+{
+  if (n == 0)
+  {
     // printf("0");
     return 0;
   }
-
-    
-  else{
-    return(n*fact(n-1));
+  else
+  {
+    return (n * fact(n - 1));
   }
 }
-int main(){
+int main()
+{
   int n;
   printf("Enter any number: ");
-  scanf("%d",&n);
-  printf("The factorial of %d is: %d",n,fact(n));
+  scanf("%d", &n);
+  printf("The factorial of %d is: %d", n, fact(n));
 }
--- lenearSearch.c
+++ lenearSearch.c
@@ -1,22 +1,28 @@
-#include<stdio.h>
-int main(){
-  int a[50],i,n,m;
+// Linear Search
+#include <stdio.h>
+int main()
+{
+  int a[50], i, n, m;
   printf("Enter the number of elements n: ");
-  scanf("%d",&n);
+  scanf("%d", &n);
   printf("Enter the elements of the array: \n");
-  for(i=0;i<n;i++){
-    scanf("%d",&a[i]);
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d", &a[i]);
   }
   printf("Enter the element to be found: ");
-  scanf("%d",&m);
+  scanf("%d", &m);
 
-  for(i=0;i<n;i++){
-    if (a[i] == m ){
-      printf("The array is found at %d position\n",i+1);
+  for (i = 0; i < n; i++)
+  {
+    if (a[i] == m)
+    {
+      printf("The array is found at %d position\n", i + 1);
       break;
     }
   }
-  if(i == n){
+  if (i == n)
+  {
     printf("The element is NOT FOUND!!!");
   }
   //  ------------OR-------------
@@ -27,14 +33,13 @@ int main(){
   //     break;
   //   }
   // }
-  
+
   // if(flag == 1){
   //   printf("The array is found at %d position\n",i+1);
   // }
   // else{
   //   printf("The element is not found in the array");
   // }
-  
-  
+
   return 0;
 }
