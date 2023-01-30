86014fac075e3ee55518eb3c3df57bb2414c5a0d
VISHNU M R <111166547+vishnumr18@users.noreply.github.com>
2023-01-30 23:04:56 +0530

    Create queueLL.c

---
+++ queueLL.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+  int info;
+  struct node *link;
+};
+
+typedef struct node *NODE;
+NODE insert_rear(NODE);
+NODE delete_front(NODE);
+void display(NODE);
+
+void main()
+{
+  NODE first;
+  int ch;
+  first = NULL;
+  while (1)
+  {
+    printf("1.Insert Rear \t 2.Delete Front\n");
+    printf("3.Display \t 4.Exit \n");
+    scanf("%d", &ch);
+    switch (ch)
+    {
+    case 1:
+      first = insert_rear(first);
+      break;
+    case 2:
+      first = delete_front(first);
+      break;
+    case 3:
+      display(first);
+      break;
+    default:
+      exit(0);
+    }
+  }
+}
+
+// Function for inserting the elements at rear end
+NODE insert_rear(NODE first)
+{
+  NODE cur, x;
+  int itm;
+  printf("Enter the item: ");
+  scanf("%d", &itm);
+  x = (NODE)malloc(sizeof(struct node));
+  x->info = itm;
+  x->link = NULL;
+  if (first == NULL)
+  {
+    return x;
+  }
+  cur = first;
+  while (cur->link != NULL)
+  {
+    cur = cur->link;
+  }
+  cur->link = x;
+  return x;
+}
+
+// Function for deleting the elements at front
+NODE delete_front(NODE first)
+{
+  NODE temp;
+  if (first == NULL)
+  {
+    printf("The list is Underflow\n");
+  }
+  temp = first;
+  temp = temp->link;
+  printf("Element deleted is:%d", first->info);
+  free(first);
+  return temp;
+}
+
+// Display the elements of the list
+void display(NODE first)
+{
+  NODE temp;
+  if (first == NULL)
+  {
+    printf("The list is empty\n");
+  }
+  printf("The content of the list are");
+  temp = first;
+  while (temp->link != NULL)
+  {
+    printf("%d", temp->info);
+    temp = temp->link;
+  }
+  printf("\n");
+}
