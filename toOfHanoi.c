d93a7296ded43409b045367d5592a8d14e1ffcd9
VISHNU M R <111166547+vishnumr18@users.noreply.github.com>
2023-01-30 21:43:17 +0530

    update the codes

--- toOfHanoi.c
+++ toOfHanoi.c
@@ -1,18 +1,23 @@
-#include<stdio.h>
+// tower of Hannoy using recrusion
+#include <stdio.h>
 
-int tower(int n,char s,char h,char d){
-  if(n==1){
-    printf("Move the disc from %c to %c\n",s,d);
+int tower(int n, char s, char h, char d)
+{
+  if (n == 1)
+  {
+    printf("Move the disc from %c to %c\n", s, d);
   }
-  else{
-    tower(n-1,s,d,h);
-    printf("The disc %d is moved from %c to %c\n",n,s,d);
-    tower(n-1,h,s,d);
+  else
+  {
+    tower(n - 1, s, d, h);
+    printf("The disc %d is moved from %c to %c\n", n, s, d);
+    tower(n - 1, h, s, d);
   }
 }
-int main(){
+int main()
+{
   int n;
   printf("Enter the number of discs: ");
-  scanf("%d",&n);
-  tower(n,'A','B','C');
+  scanf("%d", &n);
+  tower(n, 'A', 'B', 'C');
 }
